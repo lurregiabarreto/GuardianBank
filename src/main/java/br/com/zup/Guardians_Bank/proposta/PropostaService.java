00036caefc2b690d76236ac65d94abf3292eceb6
Bárbara Romero <bqromero@gmail.com>
2022-01-23 14:26:52 -0300

    Criar service para proposta

---
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaService.java
@@ -0,0 +1,12 @@
+package br.com.zup.Guardians_Bank.proposta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaService {
+
+  @Autowired
+  private PropostaRepository propostaRepository;
+
+}
