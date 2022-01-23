a9769d04173b6dc60cdd6f5bc4f2f84272a97668
Bárbara Romero <bqromero@gmail.com>
2022-01-23 14:32:52 -0300

    Criar ENUM de status da proposta

---
+++ src/main/java/br/com/zup/Guardians_Bank/enums/StatusProposta.java
@@ -0,0 +1,9 @@
+package br.com.zup.Guardians_Bank.enums;
+
+public enum StatusProposta {
+  EM_ANALISE,
+  REPROVADO,
+  APROVADO,
+  LIBERADO
+
+}
---
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaController.java
@@ -0,0 +1,14 @@
+package br.com.zup.Guardians_Bank.proposta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/propostas")
+public class PropostaController {
+
+  @Autowired
+  private PropostaService propostaService;
+
+}
