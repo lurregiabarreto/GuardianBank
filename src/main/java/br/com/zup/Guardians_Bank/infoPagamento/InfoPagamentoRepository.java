3e9474ae7ad626b63cc4ca69b6c54d5a3ab4e8fe
Bárbara Romero <bqromero@gmail.com>
2022-01-23 14:15:45 -0300

    Criar service para informacoes de pagamento

---
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Guardians_Bank.infoPagamento;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InfoPagamentoRepository extends CrudRepository <InfoPagamento, String> {
+
+}
---
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoService.java
@@ -0,0 +1,12 @@
+package br.com.zup.Guardians_Bank.infoPagamento;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoPagamentoService {
+
+  @Autowired
+  private InfoPagamentoRepository infoPagamentoRepository;
+
+}
