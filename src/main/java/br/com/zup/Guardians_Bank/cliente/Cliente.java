86d6e6d165777cd9dcdf3dadd0e4bcaa7d7f8bd4
Bárbara Romero <bqromero@gmail.com>
2022-01-23 13:47:14 -0300

    Criar interface repository para cliente

--- src/main/java/br/com/zup/Guardians_Bank/cliente/Cliente.java
+++ src/main/java/br/com/zup/Guardians_Bank/cliente/Cliente.java
@@ -2,14 +2,15 @@ package br.com.zup.Guardians_Bank.cliente;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "clientes")
 public class Cliente {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String codcli;
   private String nome;
   private String cpf;
