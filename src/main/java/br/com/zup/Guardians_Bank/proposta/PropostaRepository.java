2de4de8b87c86cb6d8cd3538cd06dc4e19a220aa
Bárbara Romero <bqromero@gmail.com>
2022-01-23 14:25:27 -0300

    Criar repository para proposta

---
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Guardians_Bank.proposta;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropostaRepository extends CrudRepository <Proposta, String> {
+
+}
