4cbfeeedf943f6a7f43070b4581693372dfa3cd4
Bárbara Romero <bqromero@gmail.com>
2022-01-23 14:11:49 -0300

    Modelar entidade para informacoes de pagamento

---
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamento.java
@@ -0,0 +1,32 @@
+package br.com.zup.Guardians_Bank.infoPagamento;
+
+import br.com.zup.Guardians_Bank.cliente.Cliente;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "infoPagamentos")
+public class InfoPagamento {
+
+  @Id
+  @GeneratedValue(generator = "system-uuid")
+  @GenericGenerator(name = "system-uuid", strategy = "uuid")
+  private String idPagamento;
+  private Proposta proposta;
+  private double valorParcela;
+  private int qtdadeDeParcelas;
+  private LocalDate dataPagamento;
+  private Cliente cliente;
+  private ProdutoFinanceiro produtoFinanceiro;
+  private double imposto;
+  private LocalDateTime dataLiberacao;
+
+}
