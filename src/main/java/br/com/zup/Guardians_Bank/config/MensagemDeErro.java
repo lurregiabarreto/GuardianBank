7ae68810b135fc6443452604b15fa2b6b6c6f243
Bárbara Romero <bqromero@gmail.com>
2022-01-24 10:25:45 -0300

    Refatorar nome da classe de mensagem de erro

--- src/main/java/br/com/zup/Guardians_Bank/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/Guardians_Bank/config/MensagemDeErro.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-public class ConfiguracaoMensagensValidacao {
+public class MensagemDeErro {
 
     @Bean
     public MessageSource mapearMensagens(){
