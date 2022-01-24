660b4716badac05c4fb24bfc05cd0a412c41d12c
Bárbara Romero <bqromero@gmail.com>
2022-01-24 11:00:53 -0300

    Criar metodo que valida o status da proposta

--- src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaService.java
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaService.java
@@ -1,5 +1,6 @@
 package br.com.zup.Guardians_Bank.proposta;
 
+import br.com.zup.Guardians_Bank.enums.StatusProposta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,20 @@ public class PropostaService {
   @Autowired
   private PropostaRepository propostaRepository;
 
+  public Proposta salvarProposta(Proposta proposta) {
+    return propostaRepository.save(proposta);
+  }
+
+  public void validarStatusProposta(Proposta proposta) {
+    if (proposta.getStatusProposta() == StatusProposta.EM_ANALISE) {
+      throw new RuntimeException(); //criar Em Analise Exception
+    }
+    if (proposta.getStatusProposta() == StatusProposta.REPROVADO) {
+      throw new RuntimeException(); //criar Proposta Recusada Exception
+    }
+    salvarProposta(proposta);
+  }
+
 }
+
+
