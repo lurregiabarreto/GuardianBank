3c21008d6cd33e143c5b3931e6d21b46f78c01bf
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-23 23:25:29 -0300

    Configurar Conversor para Model Mapper

---
+++ src/main/java/br/com/zup/Guardians_Bank/components/Conversor.java
@@ -0,0 +1,13 @@
+package br.com.zup.Guardians_Bank.components;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
