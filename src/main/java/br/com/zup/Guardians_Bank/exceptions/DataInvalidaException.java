28e2cc090cf84a5eaeaa6c81dcab7ce6c98a386c
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-24 14:58:13 -0300

    criar exception para data invalida

---
+++ src/main/java/br/com/zup/Guardians_Bank/exceptions/DataInvalidaException.java
@@ -0,0 +1,7 @@
+package br.com.zup.Guardians_Bank.exceptions;
+
+public class DataInvalidaException extends RuntimeException{
+    public DataInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
