d5b6c4e8b28c5ab40a463b075b29f01388e3ec86
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-25 11:09:53 -0300

    Calcular Juros das Parcelas

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoService.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoService.java
@@ -1,5 +1,7 @@
 package br.com.zup.Guardians_Bank.infoPagamento;
 
+import br.com.zup.Guardians_Bank.enums.ProdutoFinanceiro;
+import br.com.zup.Guardians_Bank.proposta.Proposta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,18 @@ public class InfoPagamentoService {
   @Autowired
   private InfoPagamentoRepository infoPagamentoRepository;
 
+   public void calcularJuros(Proposta proposta, InfoPagamento infoPagamento){
+     double taxa = 0;
+     double principal = proposta.getValorProposta();
+     int meses = infoPagamento.getQtdadeDeParcelas();
+
+     if (proposta.getProdutoFinanceiro().equals(ProdutoFinanceiro.PESSOAL)){
+       taxa = proposta.getProdutoFinanceiro().getTaxaDeJuros();
+     }if (proposta.getProdutoFinanceiro().equals(ProdutoFinanceiro.CONSIGNADO)){
+       taxa = proposta.getProdutoFinanceiro().getTaxaDeJuros();
+     }
+     double montante = principal * Math.pow((1 + taxa), meses);
+     double juros = montante - principal;
+
+   }
 }
