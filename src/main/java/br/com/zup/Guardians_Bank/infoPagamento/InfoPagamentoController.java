4be3ef0d313326e5cce26755228f710eceb454fd
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-26 16:47:03 -0300

    Criar metodo atualizar status

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoController.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoController.java
@@ -1,6 +1,9 @@
 package br.com.zup.Guardians_Bank.infoPagamento;
 
 import br.com.zup.Guardians_Bank.enums.StatusProposta;
+import br.com.zup.Guardians_Bank.exceptions.PropostaNaoLiberadaException;
+import br.com.zup.Guardians_Bank.infoPagamento.dto.AtualizarStatusDTO;
+import br.com.zup.Guardians_Bank.infoPagamento.dto.RespostaAtualizacaoStatusDTO;
 import br.com.zup.Guardians_Bank.infoPagamento.dto.EntradaInfoDTO;
 import br.com.zup.Guardians_Bank.infoPagamento.dto.SaidaInfoDTO;
 import br.com.zup.Guardians_Bank.proposta.Proposta;
@@ -10,29 +13,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.IllegalFormatCodePointException;
+
 @RestController
 @RequestMapping("/infos")
 public class InfoPagamentoController {
 
-  @Autowired
-  private InfoPagamentoService infoPagamentoService;
-  @Autowired
-  private PropostaService propostaService;
-  @Autowired
-  private ModelMapper modelMapper;
+    @Autowired
+    private InfoPagamentoService infoPagamentoService;
+    @Autowired
+    private PropostaService propostaService;
+    @Autowired
+    private ModelMapper modelMapper;
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public SaidaInfoDTO cadastrarInfoPagamento (@RequestBody EntradaInfoDTO entradaInfoDTO) {
-    InfoPagamento infoPagamento = modelMapper.map(entradaInfoDTO, InfoPagamento.class);
-    Proposta proposta = propostaService.buscarProposta(entradaInfoDTO.getNumeroProposta());
-    infoPagamento.setProposta(proposta);
-    propostaService.validarStatusProposta(proposta);
-    propostaService.validarDataContratacao(proposta);
-    return modelMapper.map(infoPagamentoService.salvarInfoPagamento(infoPagamento, entradaInfoDTO.getQtdadeParcelas()),
-        SaidaInfoDTO.class);
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SaidaInfoDTO cadastrarInfoPagamento(@RequestBody EntradaInfoDTO entradaInfoDTO) {
+        InfoPagamento infoPagamento = modelMapper.map(entradaInfoDTO, InfoPagamento.class);
+        Proposta proposta = propostaService.buscarProposta(entradaInfoDTO.getNumeroProposta());
+        infoPagamento.setProposta(proposta);
+        propostaService.validarStatusProposta(proposta);
+        propostaService.validarDataContratacao(proposta);
+        return modelMapper.map(infoPagamentoService.salvarInfoPagamento(infoPagamento, entradaInfoDTO.getQtdadeParcelas()),
+                SaidaInfoDTO.class);
+    }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public RespostaAtualizacaoStatusDTO atualizarStatus(@PathVariable String id, @RequestBody AtualizarStatusDTO atualizarStatusDTO) {
 
+        if (atualizarStatusDTO.getStatusProposta() != StatusProposta.APROVADO) {
+            throw new PropostaNaoLiberadaException("Proposta não liberada");
+
+        }
+        return modelMapper.map(infoPagamentoService.atualizarInfo(id), RespostaAtualizacaoStatusDTO.class);
+
+    }
 
 }
