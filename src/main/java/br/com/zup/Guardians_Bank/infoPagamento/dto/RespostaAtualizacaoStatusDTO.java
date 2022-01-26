ad074bee5c1f35d3dccd4601e58695012a10b475
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-26 16:50:46 -0300

    modificar status proposta

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/RespostaAtualizacaoStatusDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/RespostaAtualizacaoStatusDTO.java
@@ -3,6 +3,8 @@ package br.com.zup.Guardians_Bank.infoPagamento.dto;
 import br.com.zup.Guardians_Bank.enums.StatusProposta;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class RespostaAtualizacaoStatusDTO {
 
