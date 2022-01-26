4428bea9d32ecc3ae901141d0b8eeb22755d666d
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-26 16:49:12 -0300

    criar dto atualizar status

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/AtualizarStatusDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/AtualizarStatusDTO.java
@@ -3,12 +3,9 @@ package br.com.zup.Guardians_Bank.infoPagamento.dto;
 import br.com.zup.Guardians_Bank.enums.StatusProposta;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class AtualizarStatusDTO {
 
     private StatusProposta statusProposta;
-    private LocalDateTime dataLiberacao;
 
 }
