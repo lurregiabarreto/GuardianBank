94ee57350e371342fe5a99fd8ec4696288c7cc58
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-26 14:56:37 -0300

    Tratar exception Proposta não cadastrada

---
+++ src/main/java/br/com/zup/Guardians_Bank/exceptions/PropostaNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package br.com.zup.Guardians_Bank.exceptions;
+
+public class PropostaNaoEncontradaException extends RuntimeException {
+    public PropostaNaoEncontradaException(String message) {
+        super(message);
+    }
+}
