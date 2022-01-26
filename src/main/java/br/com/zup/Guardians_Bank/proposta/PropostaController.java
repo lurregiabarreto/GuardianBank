e4e1285526e48b111c7ebf9a54f90f89a178eddd
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-26 19:19:14 -0300

    Rotear Exibir opcoes de pagamentos utilizando o id da proposta

--- src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaController.java
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaController.java
@@ -1,13 +1,18 @@
 package br.com.zup.Guardians_Bank.proposta;
 
+import br.com.zup.Guardians_Bank.infoPagamento.InfoPagamento;
 import br.com.zup.Guardians_Bank.infoPagamento.InfoPagamentoService;
+import br.com.zup.Guardians_Bank.infoPagamento.dto.RetornoInfoDto;
+import br.com.zup.Guardians_Bank.proposta.dtos.OpcoesPagamentoDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
 @RequestMapping("/propostas")
 public class PropostaController {
 
@@ -18,12 +23,15 @@ public class PropostaController {
     @Autowired
     private ModelMapper modelMapper;
 
-//  @GetMapping("/{id}")
-//  public RetornoInfoDto exibirOpcoesPagamento(@PathVariable String id){
-//
-//    RetornoInfoDto retornoInfoDto = modelMapper.map(infoPagamento, RetornoInfoDto.class);
-//    return retornoInfoDto;
-//  }
+
+    @GetMapping("/{id}")
+    public OpcoesPagamentoDTO exibirOpcoesPagamento(@PathVariable String id) {
+        propostaService.validarPropostaExiste(id);
+        InfoPagamento infoPagamento = propostaService.trazerInfoPorNumProposta(id);
+        OpcoesPagamentoDTO opcoesPagamentoDTO = new OpcoesPagamentoDTO();
+        opcoesPagamentoDTO.setOpcoes(infoPagamentoService.opcoesParcelamento(infoPagamento));
+        return opcoesPagamentoDTO;
+    }
 
 
 }
