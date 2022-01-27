285256dabbe6de4a8fd615916b55be6be3f180c7
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-01-27 12:34:30 -0300

    Popular Banco de dados com diferentes escenarios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,9 @@ INSERT INTO clientes (codcli, nome, cpf, salario) VALUES (1, 'Dorayen Carrasquel
 INSERT INTO clientes (codcli, nome, cpf, salario) VALUES (2, 'Barbara Queiroz Romero', '56161432005', 1800);
 INSERT INTO clientes (codcli, nome, cpf, salario) VALUES (3, 'Lurregia Barreto Adao', '10472639048', 1900);
 
-INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('1', '2021-12-30', '1', '2', 800, '1');
-INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('2', '2022-01-16', '0', '2', 500, '2');
-INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('3', '2021-07-07', '1', '2', 900, '3');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('1','2021-12-30', '0', '2', 800, '1');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('2','2022-01-16', '1', '1', 500, '2');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('3','2022-01-07', '0', '0', 1900, '3');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('4','2021-01-07', '0', '2', 900, '1');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('5','2022-01-21', '2', '2', 5000, '2');
+INSERT INTO propostas (numero_proposta, data_proposta, produto_financeiro, status_proposta, valor_proposta, cliente_codcli) VALUES ('6','2022-01-27', '1', '2', 6000, '3');
