bebeb1e221a4248e1eaf6993247c131e4f7e0fc8
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-28 14:24:43 -0300

    Sobrescrever metodo successfulAuthentication

--- src/main/java/br/com/zup/Guardians_Bank/config/JWT/FiltroDeAutenticacaoJWT.java
+++ src/main/java/br/com/zup/Guardians_Bank/config/JWT/FiltroDeAutenticacaoJWT.java
@@ -2,6 +2,7 @@ package br.com.zup.Guardians_Bank.config.JWT;
 
 
 import br.com.zup.Guardians_Bank.config.JWT.exception.AcessoNegadoException;
+import br.com.zup.Guardians_Bank.config.Security.UsuarioLogado;
 import br.com.zup.Guardians_Bank.usuario.dto.LoginDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -41,4 +44,17 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
             throw new AcessoNegadoException();
         }
     }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UsuarioLogado usuarioLogado = (UsuarioLogado) authResult.getPrincipal();
+        String username = usuarioLogado.getUsername();
+        String id = usuarioLogado.getId();
+
+        String token = jwtComponent.gerarToken(username, id);
+
+        response.setHeader("Access-Control-Expose-Headers","Authorization");
+        response.addHeader("Authorization", "Token "+token);
+    }
+
 }
