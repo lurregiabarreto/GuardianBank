2f9d119774773c5461d4264cb4079f10683b2cda
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-28 10:18:30 -0300

    tornar rotas post publicas para a aplicacao

--- src/main/java/br/com/zup/Guardians_Bank/config/Security/ConfiguracaoDeSegurança.java
+++ src/main/java/br/com/zup/Guardians_Bank/config/Security/ConfiguracaoDeSegurança.java
@@ -1,11 +1,26 @@
 package br.com.zup.Guardians_Bank.config.Security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSegurança extends WebSecurityConfigurerAdapter {
+
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/propostas",
+            "/infoPagamentos"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+    }
 
 }
