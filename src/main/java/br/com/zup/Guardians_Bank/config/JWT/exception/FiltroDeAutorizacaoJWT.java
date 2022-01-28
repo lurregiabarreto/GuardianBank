fb0f7f5c2960c3737688a4b4e1ad98fdde36daa5
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-28 14:39:40 -0300

    Criar metodo para pegar autorizacao

--- src/main/java/br/com/zup/Guardians_Bank/config/JWT/exception/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/Guardians_Bank/config/JWT/exception/FiltroDeAutorizacaoJWT.java
@@ -1,7 +1,10 @@
 package br.com.zup.Guardians_Bank.config.JWT.exception;
 
 import br.com.zup.Guardians_Bank.config.JWT.JWTComponent;
+import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -14,5 +17,15 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+    }
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token){
+        if(!jwtComponent.tokenValido(token)){
+            throw new TokenInvalidoException();
+        }
+
+        Claims claims = jwtComponent.pegarClaims(token);
+        UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
     }
 }
