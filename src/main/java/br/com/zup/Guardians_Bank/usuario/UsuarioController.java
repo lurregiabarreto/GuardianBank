b90ba7803d033ff24026142ccdd9c1ce36d45953
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-28 11:21:43 -0300

    criar metodo cadastrar usuario

---
+++ src/main/java/br/com/zup/Guardians_Bank/usuario/UsuarioController.java
@@ -0,0 +1,23 @@
+package br.com.zup.Guardians_Bank.usuario;
+
+import br.com.zup.Guardians_Bank.usuario.dto.CadastroUsuarioDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarUsuario(@RequestBody CadastroUsuarioDTO cadastroUsuarioDTO) {
+        Usuario usuario = new Usuario();
+        usuario.setEmail(cadastroUsuarioDTO.getEmail());
+        usuario.setSenha(cadastroUsuarioDTO.getSenha());
+
+        usuarioService.salvarUsuario(usuario);
+    }
+}
