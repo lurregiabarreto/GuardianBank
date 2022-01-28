f887d416d5d6cbe55b114b2eb5687eca402240d0
Lurrégia Barreto <luhhbarreto@gmail.com>
2022-01-28 10:08:27 -0300

    alterar mensagem

--- src/main/java/br/com/zup/Guardians_Bank/config/MensagemDeErro.java
+++ src/main/java/br/com/zup/Guardians_Bank/config/MensagemDeErro.java
@@ -1,11 +1,5 @@
 package br.com.zup.Guardians_Bank.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
 public class MensagemDeErro {
 
   private String mensagemDeErro;
