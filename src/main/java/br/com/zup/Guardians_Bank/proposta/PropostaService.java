817659e27bf45fd4fcfe3829fed8f5254ae465f6
Bárbara Romero <bqromero@gmail.com>
2022-02-01 15:27:42 -0300

    Testar buscar infoPagamento por numero da proposta

--- src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaService.java
+++ src/main/java/br/com/zup/Guardians_Bank/proposta/PropostaService.java
@@ -7,7 +7,6 @@ import br.com.zup.Guardians_Bank.exceptions.PropostaNaoEncontradaException;
 import br.com.zup.Guardians_Bank.exceptions.PropostaRecusadaException;
 import br.com.zup.Guardians_Bank.infoPagamento.InfoPagamento;
 import br.com.zup.Guardians_Bank.infoPagamento.InfoPagamentoService;
-import br.com.zup.Guardians_Bank.proposta.dtos.OpcoesPagamentoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,54 +18,54 @@ import java.util.Optional;
 @Service
 public class PropostaService {
 
-    @Autowired
-    private PropostaRepository propostaRepository;
-    @Autowired
-    private InfoPagamentoService infoPagamentoService;
+  @Autowired
+  private PropostaRepository propostaRepository;
+  @Autowired
+  private InfoPagamentoService infoPagamentoService;
 
-    public Proposta validarStatusProposta(Proposta proposta) {
-        if (proposta.getStatusProposta() == StatusProposta.EM_ANALISE) {
-            throw new EmAnaliseException("Proposta em análise");
-        }
-        if (proposta.getStatusProposta() == StatusProposta.REPROVADO) {
-            throw new PropostaRecusadaException("Proposta recusada");
-        }
-        return proposta;
+  public Proposta validarStatusProposta(Proposta proposta) {
+    if (proposta.getStatusProposta() == StatusProposta.EM_ANALISE) {
+      throw new EmAnaliseException("Proposta em análise");
     }
-
-    public Proposta validarDataContratacao(Proposta proposta) {
-        LocalDate dataAtual = LocalDate.now();
-
-        LocalDate dataLimite = dataAtual.minusDays(90);
-        if (proposta.getDataProposta().isBefore(dataLimite)) {
-            throw new DataInvalidaException("Data limite excedida");
-        }
-        return proposta;
+    if (proposta.getStatusProposta() == StatusProposta.REPROVADO) {
+      throw new PropostaRecusadaException("Proposta recusada");
     }
+    return proposta;
+  }
 
-    public Proposta validarPropostaExistente(String numeroProposta) {
-        Optional<Proposta> propostaOptional = propostaRepository.findById(numeroProposta);
-        if (propostaOptional.isEmpty()) {
-            throw new PropostaNaoEncontradaException("Proposta não encontrada");
-        }
-        Proposta proposta = propostaOptional.get();
-        return proposta;
-    }
+  public Proposta validarDataContratacao(Proposta proposta) {
+    LocalDate dataAtual = LocalDate.now();
 
-    public InfoPagamento trazerInfoPorNumProposta(String id) {
-        InfoPagamento infoPagamento = new InfoPagamento();
-        Proposta proposta = validarPropostaExistente(id);
-        validarStatusProposta(proposta);
-        validarDataContratacao(proposta);
-        infoPagamento.setProposta(proposta);
-        return infoPagamento;
+    LocalDate dataLimite = dataAtual.minusDays(90);
+    if (proposta.getDataProposta().isBefore(dataLimite)) {
+      throw new DataInvalidaException("Data limite excedida");
     }
+    return proposta;
+  }
 
-    public List<InfoPagamento> exibirOpcoesValidadas(String id) {
-        InfoPagamento infoPagamento = trazerInfoPorNumProposta(id);
-        List<InfoPagamento> infoPagamentoList = new ArrayList<>();
-        infoPagamentoList.addAll(infoPagamentoService.opcoesParcelamento(infoPagamento));
-        return infoPagamentoList;
+  public Proposta validarPropostaExistente(String numeroProposta) {
+    Optional<Proposta> propostaOptional = propostaRepository.findById(numeroProposta);
+    if (propostaOptional.isEmpty()) {
+      throw new PropostaNaoEncontradaException("Proposta não encontrada");
     }
+    Proposta proposta = propostaOptional.get();
+    return proposta;
+  }
+
+  public InfoPagamento buscarInfoPorNumProposta(String id) {
+    InfoPagamento infoPagamento = new InfoPagamento();
+    Proposta proposta = validarPropostaExistente(id);
+    validarStatusProposta(proposta);
+    validarDataContratacao(proposta);
+    infoPagamento.setProposta(proposta);
+    return infoPagamento;
+  }
+
+  public List<InfoPagamento> exibirOpcoesValidadas(String id) {
+    InfoPagamento infoPagamento = buscarInfoPorNumProposta(id);
+    List<InfoPagamento> infoPagamentoList = new ArrayList<>();
+    infoPagamentoList.addAll(infoPagamentoService.opcoesParcelamento(infoPagamento));
+    return infoPagamentoList;
+  }
 
 }
--- src/test/java/br/com/zup/Guardians_Bank/proposta/PropostaServiceTest.java
+++ src/test/java/br/com/zup/Guardians_Bank/proposta/PropostaServiceTest.java
@@ -113,4 +113,22 @@ public class PropostaServiceTest {
     });
   }
 
+  @Test
+  public void testarBuscarInfoPorNumProposta() {
+    Mockito.when(propostaRepository.findById(proposta.getNumeroProposta())).thenReturn(Optional.of(proposta));
+    infoPagamento = new InfoPagamento();
+    Proposta novaProposta = propostaService.validarPropostaExistente(proposta.getNumeroProposta());
+    propostaService.validarStatusProposta(proposta);
+    String data = "2022/01/31";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDate date = LocalDate.parse(data, formatter);
+    proposta.setDataProposta(date);
+    propostaService.validarDataContratacao(proposta);
+    infoPagamento.setProposta(proposta);
+
+    Assertions.assertEquals(novaProposta, proposta);
+
+    Mockito.verify(propostaRepository, Mockito.times(1)).findById(proposta.getNumeroProposta());
+  }
+
 }
