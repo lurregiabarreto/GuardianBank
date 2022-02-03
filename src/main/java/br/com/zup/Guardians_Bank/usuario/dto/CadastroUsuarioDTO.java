ca5038f9974271de53054561e8f631a9ec130f73
Bárbara Romero <bqromero@gmail.com>
2022-02-03 06:54:52 -0300

    Aplicar validacoes na dto de cadastro do usuario

--- src/main/java/br/com/zup/Guardians_Bank/usuario/dto/CadastroUsuarioDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/usuario/dto/CadastroUsuarioDTO.java
@@ -1,26 +1,18 @@
 package br.com.zup.Guardians_Bank.usuario.dto;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
 public class CadastroUsuarioDTO {
-    private String email;
-    private String senha;
 
-    public CadastroUsuarioDTO() {
-    }
+  @NotBlank
+  @Email
+  private String email;
+  @NotBlank
+  private String senha;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
 
