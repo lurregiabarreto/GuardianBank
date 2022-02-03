05ee3c9fd5c30681bc1756e0b0fde0dca370b04f
Bárbara Romero <bqromero@gmail.com>
2022-02-03 07:41:50 -0300

    Testar cadastro com validacao de senha not blank

--- src/test/java/br/com/zup/Guardians_Bank/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/Guardians_Bank/usuario/UsuarioControllerTest.java
@@ -3,8 +3,6 @@ package br.com.zup.Guardians_Bank.usuario;
 import br.com.zup.Guardians_Bank.components.Conversor;
 import br.com.zup.Guardians_Bank.config.JWT.JWTComponent;
 import br.com.zup.Guardians_Bank.config.Security.UsuarioLoginService;
-import br.com.zup.Guardians_Bank.infoPagamento.InfoPagamento;
-import br.com.zup.Guardians_Bank.proposta.PropostaController;
 import br.com.zup.Guardians_Bank.usuario.dto.CadastroUsuarioDTO;
 import br.com.zup.Guardians_Bank.usuario.dto.UsuarioSaidaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -94,6 +92,18 @@ public class UsuarioControllerTest {
             .content(json).contentType(MediaType.APPLICATION_JSON))
         .andExpect(MockMvcResultMatchers.status().is(422));
 
+  }
+
+  @Test
+  @WithMockUser("user@user.com")
+  public void testarCadastrarUsuarioComValidacaoSenhaNotBlank() throws Exception {
+    cadastroUsuarioDTO.setSenha("    ");
+    Mockito.when((usuarioService.salvarUsuario(Mockito.any(Usuario.class)))).thenReturn(usuario);
+    String json = objectMapper.writeValueAsString(cadastroUsuarioDTO);
+
+    ResultActions resultado = mockMvc.perform(MockMvcRequestBuilders.post("/usuario")
+            .content(json).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().is(422));
   }
 
 }
