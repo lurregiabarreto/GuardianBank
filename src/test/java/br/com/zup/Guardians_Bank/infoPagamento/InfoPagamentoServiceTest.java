a1c46f9fc7c9d6e7199a42d208077c075617d3fd
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-02-03 16:55:03 -0300

    Testar opcoes de parcelamento caminho positivo

--- src/test/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoServiceTest.java
+++ src/test/java/br/com/zup/Guardians_Bank/infoPagamento/InfoPagamentoServiceTest.java
@@ -17,6 +17,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -34,6 +36,7 @@ public class InfoPagamentoServiceTest {
     private Proposta proposta;
     private InfoPagamento infoPagamento;
     private Cliente cliente;
+    private Cliente clientePobre;
 
     @BeforeEach
     public void setup() {
@@ -54,6 +57,12 @@ public class InfoPagamentoServiceTest {
         cliente.setCpf("75622060079");
         cliente.setNome("Dorayen");
         cliente.setSalario(3000);
+
+        clientePobre = new Cliente();
+        clientePobre.setCodcli("2");
+        clientePobre.setCpf("75622060079");
+        clientePobre.setNome("Carola");
+        clientePobre.setSalario(50);
 
     }
 
@@ -110,6 +119,16 @@ public class InfoPagamentoServiceTest {
                 .thenReturn(false);
         Boolean positivo = infoPagamentoService.buscarInfoPorNumeroProposta(proposta.getNumeroProposta());
         Assertions.assertFalse(positivo);
+    }
+
+    @Test
+    public void testarOpcoesParcelamentoCaminhoPositivo() {
+        proposta.setCliente(cliente);
+        infoPagamento.setProposta(proposta);
+        infoPagamentoService.opcoesParcelamento(infoPagamento);
+        List<InfoPagamento> resultado = infoPagamentoService.opcoesParcelamento(infoPagamento);
+        Assertions.assertNotNull(resultado);
+
     }
 
 }
