684bbbf5db3f8060d917aee61c9ac53c34f23824
Bárbara Romero <bqromero@gmail.com>
2022-02-03 06:33:34 -0300

    Criar método que busca todos os usuarios

--- src/main/java/br/com/zup/Guardians_Bank/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/Guardians_Bank/usuario/UsuarioService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,6 +30,11 @@ public class UsuarioService {
       return false;
     }
 
+  }
+
+  public List<Usuario> buscarTodosOsUsuarios() {
+    Iterable<Usuario> usuarios = usuarioRepository.findAll();
+    return (List<Usuario>) usuarios;
   }
 
   public void atualizarUsuario(Usuario usuario, String id) {
