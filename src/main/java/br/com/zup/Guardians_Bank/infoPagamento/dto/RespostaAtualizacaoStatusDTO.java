1d1311f6f80906fc69048c8db65e20d02d0425d8
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2022-02-08 18:18:39 -0300

    Formatar hora do LocalDataTime

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/RespostaAtualizacaoStatusDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/RespostaAtualizacaoStatusDTO.java
@@ -3,6 +3,7 @@ package br.com.zup.Guardians_Bank.infoPagamento.dto;
 import br.com.zup.Guardians_Bank.enums.StatusProposta;
 import br.com.zup.Guardians_Bank.enums.TipoDeParcela;
 import br.com.zup.Guardians_Bank.proposta.Proposta;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -17,6 +18,7 @@ public class RespostaAtualizacaoStatusDTO {
   private Integer qtdadeDeParcelas;
   private LocalDate dataPagamento;
   private double imposto;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime dataLiberacao;
   private TipoDeParcela tipoDeParcela;
 
