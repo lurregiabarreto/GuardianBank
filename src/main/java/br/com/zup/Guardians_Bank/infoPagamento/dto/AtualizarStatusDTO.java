76f00e0893c879dac07c8836e079574d70a9f678
Bárbara Romero <bqromero@gmail.com>
2022-02-08 14:50:33 -0300

    Refatorar AtualizarStatusDTO

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/AtualizarStatusDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/AtualizarStatusDTO.java
@@ -1,6 +1,7 @@
 package br.com.zup.Guardians_Bank.infoPagamento.dto;
 
 import br.com.zup.Guardians_Bank.enums.StatusProposta;
+import br.com.zup.Guardians_Bank.enums.TipoDeParcela;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotNull;
 @Data
 public class AtualizarStatusDTO {
 
-  private StatusProposta statusProposta;
+  private int numeroParcelas;
+  private TipoDeParcela tipoDeParcela;
 
 }
