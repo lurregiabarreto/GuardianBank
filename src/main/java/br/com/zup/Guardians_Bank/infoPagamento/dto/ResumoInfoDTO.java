4475203921db37ef0bc65ad80bee9f8beb347d98
Bárbara Romero <bqromero@gmail.com>
2022-02-08 15:01:04 -0300

    Refatorar DTOs de atualizacao

--- src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/ResumoInfoDTO.java
+++ src/main/java/br/com/zup/Guardians_Bank/infoPagamento/dto/ResumoInfoDTO.java
@@ -1,5 +1,6 @@
 package br.com.zup.Guardians_Bank.infoPagamento.dto;
 
+import br.com.zup.Guardians_Bank.enums.TipoDeParcela;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,9 +9,11 @@ import java.time.LocalDateTime;
 @Data
 public class ResumoInfoDTO {
 
+  private String idPagamento;
   private double valorParcela;
   private Integer qtdadeDeParcelas;
   private LocalDate dataPagamento;
   private LocalDateTime dataLiberacao;
+  private TipoDeParcela tipoDeParcela;
 
 }
